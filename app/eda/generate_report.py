b3360173489ce00712f83d5b979341b7fef77052
u_m11r9 <egolubov@alfabank.ru>
2023-05-20 22:59:30 +0300

    Add EDA report.html

--- app/eda/generate_report.py
+++ app/eda/generate_report.py
@@ -3,9 +3,10 @@ from pandas_profiling import ProfileReport
 
 
 def main():
-    incomes_raw_df = pd.read_excel('../static/terminal_data_hackathon v4.xlsx',
-                                   sheet_name='Incomes', engine='openpyxl')
-    profile = ProfileReport(incomes_raw_df)
+    df = pd.read_excel('../static/terminal_data_hackathon v4.xlsx',
+                       sheet_name='Incomes', engine='openpyxl')
+    df.drop(df.columns[list(range(10, len(df.columns)))], axis=1, inplace=True)
+    profile = ProfileReport(df)
 
     profile.to_file(output_file='report.html')
 
